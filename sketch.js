03a29ceb79aae2cda05978a18c1da7312ca5ceef
JuanF9910 <juan.zapata97@udea.edu.co>
2023-10-05 17:14:56 -0500

    update-j

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 let g = 9.8; // gravedad
-let a = 20; // radio principal de la elipse
-let b = 5; // radio secundario de la elipse
+let a = 30; // radio principal de la elipse
+let b = 10; // radio secundario de la elipse
 let w = 50; // ancho del bloque
 let h = 50; // alto del bloque
-let dt = 1 / 30; // esto es un número que se llama dt y que es el tiempo que pasa entre cada frame.
+let dt = 1 / 3; // tiempo entre cada frame (en milisegundos)
 let l = 200; // longitud de la cuerda
-let beta = 0.1; // factor de fricción.
+let beta = 0.1; // factor de fricción
+let L=400; // distancia desde donde se dispara la bala
 
 let bloque; // objeto bloque
 let bala; // objeto bala
@@ -15,47 +16,45 @@ let m = 10; // masa de la bala
 let M = 50; // masa del bloque
 let v_b = 60; // velocidad de la bala
 
-
 let omega = Math.sqrt(g / l);
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight); //
-  frameRate(30); // esto es para que el programa corra a 30 fps
-  bloque = new Bloque(-w / 2, -h / 2, M); // creo el objeto bloque
-  cuerda = new Cuerda(0, 0); // creo el objeto cuerda
-  bala = new Bala(v_b, m); // creo el objeto bala
-
-};
+  frameRate(200); // 30 fps
+  bloque = new Bloque(-w / 2, l-h / 2, M); // crea el objeto bloque
+  cuerda = new Cuerda(0, 0); // crea el objeto cuerda
+  bala = new Bala(v_b, m); // crea el objeto bala
+}
 
 function draw() {
-  translate(windowWidth / 2, windowHeight / 2); // esto es para que el origen de coordenadas esté en la esquina inferior izquierda.
-  background(94, 205, 206); // color que tiene el fondo canvas (gris).
+  translate(windowWidth / 2, windowHeight / 2); // coloca el origen en el centro
+  background(255); // fondo blanco
 
+  
   bloque.show();
   cuerda.show();
-  bala.show();
 
-
+  
   if (bala.colision()) {
     bloque.move();
     cuerda.move(bloque.x, bloque.y);
-  }else{
+  } else {
     bala.move();
+    bala.show();
   }
-};
+}
 
-let Bloque = function (x, y, m) { //clase bloque
-
+let Bloque = function (x, y, m) {
   this.x = x;
   this.y = y;
   this.m = m;
 
-  let t = 0; //tiempo
+  let t = 0;
   let theta = 0;
 
   this.show = function () {
     noStroke();
-    fill(0);
+    fill(139, 69, 19); // Color marrón para el bloque
     rect(this.x, this.y, w, h);
   }
 
@@ -69,15 +68,22 @@ let Bloque = function (x, y, m) { //clase bloque
 }
 
 let Cuerda = function (x1, y1) {
-
   this.x0 = 0;
   this.y0 = 0;
   this.x1 = x1;
   this.y1 = y1;
 
   this.show = function () {
-    stroke(255, 0, 0);
-    line(this.x0, this.y0, this.x1 + w / 2, this.y1 + h / 2);  //para que esté en el centro del bloque se le suman w/2 y h/2
+    stroke(255, 0, 0); // Color rojo para la cuerda
+    strokeWeight(5);
+
+    if(this.y1==0){
+      line(this.x0, this.y0, this.x1,  -h /2 + l);
+
+    } else{
+      line(this.x0, this.y0, this.x1 + w / 2, this.y1 + h / 2);
+    }
+    
   }
 
   this.move = function (x, y) {
@@ -86,8 +92,7 @@ let Cuerda = function (x1, y1) {
   }
 }
 
-let Bala = function (vx, m) { //clase balas
-
+let Bala = function (vx, m) {
   this.vxb = vx;
   this.m = m;
 
@@ -95,17 +100,16 @@ let Bala = function (vx, m) { //clase balas
 
   this.show = function () {
     noStroke();
-    fill(255);
-    ellipse(-l + xb, l, a, b);
+    fill(0, 0, 255); // Color azul para la bala
+    ellipse(-L + xb, l, a, b);
   }
 
-  this.move = function () {
+  this.move = function (val) {
     xb = xb + this.vxb * dt;
-
   }
 
   this.colision = function () {
-    if (-l+xb >= -w/2) {
+    if (-L + xb >= -w / 2) {
       return true;
     }
   }
