d2fdbdf9c2be5edd15cd924f16d8b2d02485b2ad
DiegoGarro3 <53834603+DiegoGarro3@users.noreply.github.com>
2023-10-22 23:14:45 -0500

    Update sketch.js
    Now the simulation re-starts when the slider values are changed

--- sketch.js
+++ sketch.js
@@ -1,71 +1,99 @@
-let g = 9.8; // gravedad
-let a = 40; // radio principal de la elipse
-let b = 15; // radio secundario de la elipse
-let w = 80; // ancho del bloque
-let h = 80; // alto del bloque
-let dt = 1 / 3; // tiempo entre cada frame (en milisegundos)
-let l = 200; // longitud de la cuerda
-let beta = 0.1; // factor de fricción
-let L = 750; // distancia desde donde se dispara la bala
+let g = 9.8; // gravity
+let a = 40; // major axis of the ellipse
+let b = 15; // minor axis of the ellipse
+let w = 80; // block width
+let h = 80; // block height
+let dt = 1 / 3; // time between each frame (in milliseconds)
+let l = 200; // length of the string
+let beta = 0.1; // friction factor
+let L = 750; // distance from where the bullet is fired
 
-let bloque; // objeto bloque
-let bala; // objeto bala
-let cuerda; // objeto cuerda
-let soporte; 
+let bloque; // block object
+let bala; // bullet object
+let cuerda; // string object
+let soporte;
 
-let m; // masa de la bala
-let M; // masa del bloque
-let v_b; // velocidad de la bala
+let m; // bullet mass
+let M; // block mass
+let v_b; // bullet velocity
 
 let omega = Math.sqrt(g / l);
 
 let mSlider, MSlider, v_bSlider, resetButton;
 
-let ended = false;
+//let ended = false;
 
-let TT=[];
-let HH=[];
+let T = [];
+let H = [];
+
+let mValueElement, MValueElement, v_bValueElement;
 
 function setup() {
-
-  canvas = createCanvas(windowWidth, windowHeight); // crea el lienzo
+  
+  m = 0.1; // Initial value for bullet mass (adjust as needed)
+  M = 50; // Initial value for block mass (adjust as needed)
+  v_b = 40; // Initial value for bullet velocity (adjust as needed)
+  canvas = createCanvas(windowWidth, windowHeight);// crea el lienzo
   frameRate(200); // establece la velocidad de los frames
   // Crea los deslizadores con etiquetas en formato LaTeX
-  mSlider = createSlider(1, 10, m, 1); // crea el deslizador de la masa de la bala con valores entre 1 y 10 y un incremento de 1 
-  positionSlider(mSlider, 'Masa bala [kg]', 20, 20, 250); // Aumenta el último argumento para un mayor ancho del deslizador 
+  mSlider = createSlider(1, 10, 5, 1);// crea el deslizador de la masa de la bala con valores entre 1 y 10 y un incremento de 1 
+  mValueElement = createSpan(mSlider.value()); //crea el valor que se va a cambiar en la pantalla (the value element for v_b)
+  positionSlider(mSlider, 'Bullet Mass [kg]', 20, 20, 250, mValueElement);// Aumenta el último argumento para un mayor ancho del deslizador
 
-  MSlider = createSlider(10, 100, M, 1); // crea el deslizador de la masa del bloque con valores entre 10 y 100 y un incremento de 1
-  positionSlider(MSlider, 'Masa péndulo [kg]', 20, 50, 250); // Aumenta el último argumento para un mayor ancho del deslizador 
+  MSlider = createSlider(10, 100, 50, 1);// crea el deslizador de la masa del bloque con valores entre 10 y 100 y un incremento de 1
+  MValueElement = createSpan(MSlider.value());
+  positionSlider(MSlider, 'Block Mass [kg]', 20, 50, 250, MValueElement);// Aumenta el último argumento para un mayor ancho del deslizador 
 
-  v_bSlider = createSlider(0, 100, v_b, 1); // crea el deslizador de la velocidad de la bala con valores entre 0 y 100 y un incremento de 1
-  positionSlider(v_bSlider, 'rapidéz bala [m/s]', 20, 80, 250); // Aumenta el último argumento para un mayor ancho del deslizador
+  v_bSlider = createSlider(0, 100, 40, 1);// crea el deslizador de la velocidad de la bala con valores entre 0 y 100 y un incremento de 1
+  v_bValueElement = createSpan(v_bSlider.value());
+  positionSlider(v_bSlider, 'Bullet Velocity [m/s]', 20, 80, 250, v_bValueElement); // Aumenta el último argumento para un mayor ancho del deslizador
+
+  mSlider.input(() => { //crea un listener que ejecuta una accion cuando el valor en el slide es cambiado, actualiza la variable global m y actualiza el elemento de valor de m que sale en la pantalla, finalmente re-inicia la simulacion con este nuevo valor m
+  const m = mSlider.value();
+  mValueElement.html(m);
+  
+  resetAnimation(m,M,v_b);
+});
+  
+MSlider.input(() => {
+  M = MSlider.value();
+  MValueElement.html(M);
+
+  resetAnimation(m,M,v_b);
+});
+
+v_bSlider.input(() => {
+  v_b = v_bSlider.value();
+  v_bValueElement.html(v_b);
+
+  resetAnimation(m,M,v_b);
+});
 
   // Crea el botón de reinicio
-  resetButton = createButton('Iniciar'); // crea el botón de reinicio con el texto 'Reiniciar' 
-  resetButton.mousePressed(resetAnimation); // llama a la función resetAnimation cuando se presiona el botón de reinicio 
-  resetButton.style('font-size', '20px'); // Cambia el tamaño del texto en el botón
+  resetButton = createButton('Start');// crea el botón de reinicio con el texto 'Reiniciar' 
+  resetButton.mousePressed(resetAnimation(m,M,v_b)); // llama a la función resetAnimation cuando se presiona el botón de reinicio 
+  resetButton.style('font-size', '20px');// Cambia el tamaño del texto en el botón
   resetButton.size(120, 40); // Cambia el tamaño del botón en píxeles
   resetButton.position(windowWidth / 2 - 50, 100); // coloca el botón de reinicio en la posición (20, 110)
-
-  resetButton = createButton('Guardar'); // crea el botón de reinicio con el texto 'Reiniciar' 
+  
+    resetButton = createButton('Guardar'); // crea el botón de reinicio con el texto 'Reiniciar' 
   resetButton.mousePressed(savedata); // llama a la función resetAnimation cuando se presiona el botón de reinicio 
   resetButton.style('font-size', '20px'); // Cambia el tamaño del texto en el botón
   resetButton.size(130, 50); // Cambia el tamaño del botón en píxeles
   resetButton.position(windowWidth / 2 + 200, 100); // coloca el botón de reinicio en la posición (20, 110)
 
-
-  soporte=new Soporte();
+  soporte = new Soporte();
   cuerda = new Cuerda(0, 0); // crea el objeto cuerda
-  bloque = new Bloque(-w / 2, l - h / 2,  MSlider.value()); // crea el objeto bloque
-  bala = new Bala(v_bSlider.value(), mSlider.value()); // crea el objeto bal
-
-
+  bloque = new Bloque(-w / 2, l - h / 2, MSlider.value()); // crea el objeto bloque
+  bala = new Bala(v_bSlider.value(), mSlider.value()); // crea el objeto bala
 }
+
+
 
 function draw() {
 
   translate(windowWidth / 2, windowHeight / 2); // coloca el origen en el centro
-  background(250, 250, 210); // fondo blanco
+    background(207, 150, 450); // fondo blanco
   // Actualiza los valores de M, m y v_b desde los deslizadores
   M = MSlider.value(); // actualiza el valor de M desde el deslizador de la masa del bloque 
   m = mSlider.value(); // actualiza el valor de m desde el deslizador de la masa de la bala
@@ -77,7 +105,7 @@ function draw() {
 
   if(bala.colision()){ // si la bala colisiona con el bloque
     t = 0;
-    bloque.move(); // mueve el bloq
+    bloque.move(); // mueve el bloque
     cuerda.move(bloque.x, bloque.y); 
 
   } else { // si la bala no colisiona con el bloque
@@ -88,10 +116,10 @@ function draw() {
 
 
 
-let BloqueV = function (x, y, m, T, H) { // crea el objeto bloque
+let BloqueV = function (x, y, MB, T, H) { // crea el objeto bloque
   this.x = x; // posición en x del bloque 
   this.y = y; // posición en y del bloque 
-  this.m = m; // masa del bloque
+  this.M = MB; // masa del bloque
 
   let t = 0; // tiempo 
   let theta = 0; // ángulo de oscilación del bloque 
@@ -151,11 +179,12 @@ let BloqueV = function (x, y, m, T, H) { // crea el objeto bloque
   }
 }
 
-let Bloque=function(x, y, m){ // crea el objeto bloque
-
+let Bloque=function(x, y, Mb){ // crea el objeto bloque
+    //console.log(M)
+    //console.log(m)
   this.x = x; // posición en x del bloque 
   this.y = y; // posición en y del bloque 
-  this.m = m; // masa del bloque
+  this.M = Mb; // masa del bloque
   
   let t = 0; // tiempo 
   let theta = 0; // ángulo de oscilación del bloque
@@ -175,6 +204,8 @@ let Bloque=function(x, y, m){ // crea el objeto bloque
     this.y = l * Math.cos(theta) - h / 2; // actualiza la posición en y del bloque
     HH.push(l * (1 - Math.cos(theta)));
     t = t + dt; // incrementa el tiempo en dt
+      //console.log(M)
+      //console.log(m)
   }
 
 
@@ -187,7 +218,7 @@ let Cuerda = function(x1, y1){ // crea el objeto cuerda
   this.y1 = y1; // posición en y del extremo libre de la cuerda
 
   this.show = function () { // muestra la cuerda
-    stroke(255, 0, 0); // Color rojo para la cuerda
+    stroke(255, 255, 255); // Color rojo para la cuerda
     strokeWeight(5); // grosor de la cuerda  en píxeles
 
     if (this.y1 == 0) { // si la cuerda está en reposo
@@ -216,9 +247,9 @@ let Soporte= function(){
   }
 }
 
-let Bala = function (vx, m) { // crea el objeto bala
+let Bala = function (vx, mb) { // crea el objeto bala
   this.vxb = vx; // velocidad en x de la bala
-  this.m = m; // masa de la bala
+  this.m = mb; // masa de la bala
 
   let xb = 0; // posición en x de la bala
 
@@ -239,7 +270,7 @@ let Bala = function (vx, m) { // crea el objeto bala
   }
 }
 
-function positionSlider(slider, label, x, y, sliderWidth) { // crea un deslizador con una etiqueta en formato LaTeX en la posición (x, y)
+function positionSlider(slider, label, x, y, sliderWidth,valueElement) { // crea un deslizador con una etiqueta en formato LaTeX en la posición (x, y)
   slider.position(x, y); // Ajusta la posición del deslizador
   slider.style('width', sliderWidth + 'px'); // Ajusta el ancho del deslizador
 
@@ -247,7 +278,7 @@ function positionSlider(slider, label, x, y, sliderWidth) { // crea un deslizado
   labelElement.position(x + sliderWidth + 10, y); // Ajusta la posición de la etiqueta
   labelElement.style('font-size', '25px'); // Aumenta el tamaño de la fuente de la etiqueta
 
-  let valueElement = createSpan(slider.value()); // Crea un elemento de texto con el valor numérico del deslizador
+  //let valueElement = createSpan(slider.value()); // Crea un elemento de texto con el valor numérico del deslizador
   valueElement.position(x + sliderWidth + 250, y); // Ajusta la posición del valor numérico
   valueElement.style('font-size', '25px'); // Aumenta el tamaño de la fuente del valor numérico
   valueElement.style('font-weight', 'bold'); // Opcional: haz que el valor sea más visible
@@ -258,16 +289,20 @@ function positionSlider(slider, label, x, y, sliderWidth) { // crea un deslizado
   });
 }
 
-function resetAnimation() { // reinicia la animación
+function resetAnimation(m,M,v_b) { // reinicia la animación
 
-  m=mSlider.value();
-  M=MSlider.value();
-  v_b=v_bSlider.value();
+  //m=mSlider.value();
+  //M=MSlider.value();
+  //v_b=v_bSlider.value();
 
   // Restablece los objetos y valores necesarios
+    //console.log(M)
   bloque = new Bloque(-w / 2, l - h / 2, M);
+    //console.log(M)
   cuerda = new Cuerda(0, 0);
   bala = new Bala(v_b, m);
+  
+
 
   // Restablece los deslizadores a sus valores iniciales
 }
@@ -286,4 +321,4 @@ function savedata() { // reinicia la animación
   bala = new Bala(v_b, m);
 
   // Restablece los deslizadores a sus valores iniciales
-};
+}
